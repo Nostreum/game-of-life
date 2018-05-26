690d6498da07c858f005d20a35f0b95a4a879f6c
Fargeix Yohan <fargeixyohan@gmail.com>
2018-05-27 00:53:43 +0200

    In progress : SIMD version of nex generation

--- src/board.c
+++ src/board.c
@@ -1,5 +1,7 @@
 #include <board.h>
 #include <file.h>
+#include <emmintrin.h>
+#include <simd.h>
 
 int** alloc_board(int height, int width) {
 
@@ -62,14 +64,12 @@ void next_generation(int **board, int height, int width) {
 
             int cs = board_c[i][j];
 
-            int sum_dead  = 0;
             int sum_alive = 0;
             for (int k=i-1; k<=i+1; k++) {
                 for (int l=j-1; l<=j+1; l++) {
                     if (k == i && l == j) continue;
           
-                    if      (board_c[k][l] == DEAD)   sum_dead++;
-                    else if (board_c[k][l] == ALIVE)  sum_alive++;
+                    if (board_c[k][l] == ALIVE)  sum_alive++;
                 }
             }
 
@@ -95,6 +95,77 @@ void next_generation(int **board, int height, int width) {
 
     free(board_c);
 }
+
+// TODO: In progress
+//void next_generation_simd(int_a **board, int height, int width) {
+//
+//    int_a **board_c = copy_board(board, height, width);
+//
+//    vint32 a0, a1, a2;
+//    vint32 b0, b1, b2;
+//    vint32 c0, c1, c2;
+//   
+//    vint32 l0, l1, l2;
+//    vint32 r0, r1, r2;
+//    vint32 s0, s1, s2;
+//
+//    vint32 m0 = vint32_set(0, 0xFFFF, 0, 0);
+//    vint32 m1 = vint32_set(0, 0, 0xFFFF, 0);
+//    vint32 mask_j = vint32_set(0xFFFF, 0xFFFF, 0xFFFF, 0);
+// 
+//    for (int i=0; i<height; i++) {
+//        
+//        a0 = vint32_load2D(board, 0, 0);
+//        a1 = vint32_load2D(board, 1, 0);
+//        a2 = vint32_load2D(board, 2, 0);
+//
+//        b0 = vint32_load2D(board, 0, 1);
+//        b1 = vint32_load2D(board, 1, 1);
+//        b2 = vint32_load2D(board, 2, 1);
+//
+//        for (int j=0; j<width/4; j++) {
+//
+//            /* Load new column */
+//            c0 = vint32_load2D(board, i-1, j+1);
+//            c1 = vint32_load2D(board, i,   j+1);
+//            c2 = vint32_load2D(board, i+1, j+1);
+//
+//            /* left shift */
+//            l0 = vint32_left(a0, b0);
+//            l1 = vint32_left(a1, b1);
+//            l2 = vint32_left(a2, b2);
+//
+//            /* right shift */
+//            r0 = vint32_right(b0, c0);
+//            r1 = vint32_right(b1, c1);
+//            r2 = vint32_right(b2, c2);
+//
+//            /* Compute 1st point */
+//            s0 = vint32_add3(l0, l1, l2);
+//            s0 = vint32_sub(s0, vint32_and(b1, m0));
+//            s0 = vint32_and(s0, mask_j);
+//            s0 = vint32_hadd(s0);
+//            s0 = vint32_hadd(s0);
+//
+//            /* Compute 2nd point */
+//
+//            /* Compute 3th point */
+//
+//            /* Compute 4th point */
+//
+//            /* Register rotation */
+//            a0 = b0;
+//            a1 = b1;
+//            a2 = b2;
+//
+//            b0 = c0;
+//            b1 = c1;
+//            b2 = c2;
+//            
+//        }
+//    }
+//
+//}
 
 void update_board(int **board, int x, int y, int new_state) {
     board[x][y] = new_state;
